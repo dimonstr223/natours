95b592f5d10af2397f33227bda70a763b18a3b4f
dimonstr223 <dimstukmikolu@gmail.com>
2022-12-30 10:12:52 +0300

    Delete tour route added

--- app.js
+++ app.js
@@ -82,6 +82,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id >= tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App runnig on port ${port}...`);
